137af5de4f6c5dd11734a9c972d9569820ad2ab1
Md Tarek Miah <tarekmucse2@gmail.com>
2026-04-06 03:16:14 +0600

    updated

---
+++ script.js
@@ -0,0 +1,28 @@
+// header
+
+let menu = document.querySelector('#menubtn');
+let nav = document.querySelector('nav');
+
+menu.onclick = () =>{
+    menu.classList.toggle('fa-x');
+    nav.classList.toggle('active')
+}
+window.onscroll = () =>{
+    menu.classList.remove('fa-x');
+    nav.classList.remove('active')
+}
+
+
+// typing text
+
+var typed = new Typed(".typing",{
+    strings : ["Competitive Programmer","Provlem Solver","Aspiring software engineer"],
+    typeSpeed : 150,
+    backSpeed : 150,
+    loop : true
+})
+
+const form = document.querySelector('form');
+form.onsubmit = () => {
+    alert("Thank you! Your email has been sent successfully.");
+};
